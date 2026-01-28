e119cc21338c3fd80a53a6eee825a2f2b1141a8e
nick0221 <nick022190@gmail.com>
2026-01-28 13:21:14 +0800

    added dialog create for permission

---
+++ resources/js/pages/rolesPermissions/dialog/dialog-add-permission.tsx
@@ -0,0 +1,86 @@
+import { useForm } from '@inertiajs/react';
+import { DialogClose } from '@radix-ui/react-dialog';
+import { route } from 'ziggy-js';
+
+import InputError from '@/components/input-error';
+import { Button } from '@/components/ui/button';
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogFooter,
+    DialogHeader,
+    DialogTitle,
+} from '@/components/ui/dialog';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+
+interface DialogAddRoleProps {
+    open: boolean;
+    setOpen: (open: boolean) => void;
+}
+
+export default function DialogAddPermission({
+    open,
+    setOpen,
+}: DialogAddRoleProps) {
+    const { data, setData, post, processing, errors, reset } = useForm<{
+        name: string;
+    }>({
+        name: '',
+    });
+
+    const handleSubmit = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        post(route('permissions.store'), {
+            onSuccess: () => {
+                reset();
+                //toast.success('New role has been successfully created!');
+                setOpen(false);
+            },
+            preserveScroll: true,
+        });
+    };
+
+    return (
+        <Dialog open={open} onOpenChange={setOpen}>
+            <DialogContent>
+                <form onSubmit={handleSubmit}>
+                    <DialogHeader>
+                        <DialogTitle>Create</DialogTitle>
+                        <DialogDescription>
+                            Register a new permission
+                        </DialogDescription>
+                    </DialogHeader>
+
+                    <div className="grid gap-3 py-4">
+                        <div className="flex flex-col items-start gap-2">
+                            <Label htmlFor="name">Name</Label>
+                            <Input
+                                id="name"
+                                className="col-span-3"
+                                value={data.name}
+                                onChange={(e) =>
+                                    setData('name', e.target.value)
+                                }
+                            />
+                        </div>
+                        <InputError message={errors.name} />
+                    </div>
+
+                    <DialogFooter>
+                        <DialogClose asChild>
+                            <Button variant="outline" type="button">
+                                Cancel
+                            </Button>
+                        </DialogClose>
+                        <Button type="submit" disabled={processing}>
+                            {processing ? 'Saving...' : 'Save'}
+                        </Button>
+                    </DialogFooter>
+                </form>
+            </DialogContent>
+        </Dialog>
+    );
+}
