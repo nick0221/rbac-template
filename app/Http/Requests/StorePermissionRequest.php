87804c469ff2c502bf8ccac82713b018b47a5b51
nick0221 <nick022190@gmail.com>
2026-02-01 02:57:36 +0800

    refactor the storePermissionRequest

--- app/Http/Requests/StorePermissionRequest.php
+++ app/Http/Requests/StorePermissionRequest.php
@@ -38,7 +38,7 @@ class StorePermissionRequest extends FormRequest
     protected function prepareForValidation(): void
     {
         $this->merge([
-            'name' => Str::slug(trim($this->name)),
+            'name' => str_replace(' ', '.', trim($this->name)),
         ]);
     }
 
